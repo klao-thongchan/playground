46c6506e337121866d3648397920041c10856279
YorYee <klaogeekz@gmail.com>
2022-01-29 10:14:37 +0700

    20220129

--- keepgreen.py
+++ keepgreen.py
@@ -1,3 +1,4 @@
 #betterkeepitgreen
 #MLK V                       MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMDXSSS GF     J D  BMUKK,KBBBBB NNC                     ฝ_ใF   zy bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccc l ำำำำำำใใใใใใใใใใใใใใใใใใใใใใใใใใใ.fsfFCCXXXXXXXXXXXXXXXXXXXXXXX,MJ,MJCCZVBvcx  8 MMN 
 #above is arkin try to type something encrypted
+#20220129
