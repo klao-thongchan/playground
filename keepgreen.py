faa95f22b22bd5f4bc89b57292d75bbd1aa6fb2d
YorYee <klaogeekz@gmail.com>
2022-02-21 06:43:06 +0700

    g

--- keepgreen.py
+++ keepgreen.py
@@ -2,3 +2,4 @@
 #MLK V                       MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMDXSSS GF     J D  BMUKK,KBBBBB NNC                     ฝ_ใF   zy bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccc l ำำำำำำใใใใใใใใใใใใใใใใใใใใใใใใใใใ.fsfFCCXXXXXXXXXXXXXXXXXXXXXXX,MJ,MJCCZVBvcx  8 MMN 
 #above is arkin try to type something encrypted
 #20220129
+#20220221
