fa40de954615b00fe874146a7daad0907423955b
YorYee <klaogeekz@gmail.com>
2022-05-21 08:44:16 +0700

    learn more

---
+++ learning/switchcase.py
@@ -0,0 +1,29 @@
+def http_error(status):
+    match status:
+        case 400:
+            return "Bad request"
+        case 401 | 403:
+            return "Not allowed"
+        case 404:
+            return "Not found"
+        case 418:
+            return "I'm a teapot"
+        case _:
+            return "Something's wrong with the Internet"
+
+
+# point is an (x, y) tuple
+def check_point(point):
+    x = 10
+    y = 26
+    match point:
+        case (0, 0):
+            print("Origin")
+        case (0, y):
+            print(f"Y={y}")
+        case (x, 0):
+            print(f"X={x}")
+        case (x, y):
+            print(f"X={x}, Y={y}")
+        case _:
+            raise ValueError("Not a point")
